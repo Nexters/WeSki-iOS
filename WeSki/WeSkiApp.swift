4dbb9a89122247b72f4e960cc6da9ee8033b699b
kokojong <duwhd0349@kookmin.ac.kr>
2024-07-17 22:29:23 +0900

    [Setting] project 기본 세팅

---
+++ WeSki/WeSkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSkiApp.swift
+//  WeSki
+//
+//  Created by kokojong on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
