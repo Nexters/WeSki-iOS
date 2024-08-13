b5e78c9ee37315f82b718053280fc59660ba07b7
kokojong <duwhd0349@kookmin.ac.kr>
2024-08-13 23:40:40 +0900

    [Model] 임시 Data 모델 추가

---
+++ WeSki/Model/Resort.swift
@@ -0,0 +1,14 @@
+//
+//  Resort.swift
+//  WeSki
+//
+//  Created by kokojong on 8/13/24.
+//
+
+import Foundation
+
+struct Resort {
+    let name: String
+    let weather: Weather
+//    let weeklyWeather: [WeekWeather]
+}
---
+++ WeSki/Model/Weather.swift
@@ -0,0 +1,12 @@
+//
+//  Weather.swift
+//  WeSki
+//
+//  Created by kokojong on 8/13/24.
+//
+
+import Foundation
+
+struct Weather {
+    let tmp: Int // 현재 온도 TODO: 이름 고민
+}
